614eac56a849c73706064eed689a919fb6b4708f
Cristina Dege <cris.dege0813@gmail.com>
2022-10-26 12:32:01 -0400

    timer complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,9 +50,64 @@ const questions = [
 ];
 
 var startButton = document.getElementById('start-btn');
+var startScreen = document.getElementById('start-screen');
+var questionsContainer = document.getElementById('question-container');
+
+var score = 0;
+var randomQuestions;
+var setQuestions = 0;
 
 startButton.addEventListener('click', startQuiz);
 
+var count = document.querySelector('#countdown');
+
+var secondsLeft = 60;
+var timer;
+function timer() {
+  timer = setInterval(function () {
+    secondsLeft--;
+    count.textContent = secondsLeft;
+    console.log(secondsLeft);
+
+    if (secondsLeft === 0) {
+      clearInterval(timer);
+      alert('You ran out of time!');
+    }
+  }, 1000);
+}
+
 function startQuiz() {
   console.log('started');
+  startButton.remove();
+  startScreen.remove();
+  questionsContainer.classList.remove('hide');
+  if (setQuestions < 5) {
+    nextQuestion(questions[setQuestions]);
+    localStorage.setItem('mostRecentScore', score);
+  } else {
+    endQuiz();
+  }
+  timer();
+}
+
+function nextQuestion(question) {
+  console.log(question);
+  document.querySelector('#question').textContent = question.question;
+  // text for buttons
+  document.querySelector('#btn1').textContent =
+    '1. ' + question.answers[0].text;
+  document.querySelector('#btn2').textContent =
+    '2. ' + question.answers[1].text;
+  document.querySelector('#btn3').textContent =
+    '3. ' + question.answers[2].text;
+  document.querySelector('#btn4').textContent =
+    '4. ' + question.answers[3].text;
+}
+
+function endQuiz() {
+  startButton.remove();
+  startScreen.remove();
+  questionsContainer.remove();
+  endScreen.classList.remove('hide');
+  clearInterval(timer);
 }
